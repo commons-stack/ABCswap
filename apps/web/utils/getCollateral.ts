cbccb850bbddbdc33fc2c1085c44f8db074a474f
Jordi Planas <jorvixsky@proton.me>
2023-08-22 15:20:00 +0200

    Refactored getCollateral

--- apps/web/utils/getCollateral.ts
+++ apps/web/utils/getCollateral.ts
@@ -3,27 +3,18 @@ import bondingCurveAbi from './abi/augmented-bonding-curve.json'
 import { readContract } from '@wagmi/core'
 import { Abi } from 'viem';
 
-export async function getCollateral() {
+export async function getCollateral(): Promise<[boolean, bigint, bigint, number]> {
     
     if (!knownContracts) throw new Error('Known contracts not found');
     if (!knownContracts[100].COLLATERAL_TOKEN) throw new Error('Collateral token contract not found');
     if (!knownContracts[100].BONDING_CURVE) throw new Error('Bonding Curve contract not found');
 
-    const bondingCurveContract = {
+    const [ whitelist, virtualBalance, virtualSupply, reserveRatio ] = await readContract({
         address: knownContracts[100].BONDING_CURVE as `0x${string}`,
-        abi: bondingCurveAbi as Abi
-    }
-
-    const collateral = readContract({
-        ...bondingCurveContract,
+        abi: bondingCurveAbi as Abi,
         functionName: 'getCollateralToken',
         args: [knownContracts[100].COLLATERAL_TOKEN as `0x${string}`]
-    }).then((data) => {
-        return data;
-    }).catch(err => {
-        console.error(err);
-    });
+    }) as [boolean, bigint, bigint, number];
 
-    return collateral;
-   
+    return [ whitelist, virtualBalance, virtualSupply, reserveRatio ];
 }
