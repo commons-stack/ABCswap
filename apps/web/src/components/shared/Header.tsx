3f04f522541344dcaaa6c26cc9dcf3c3faba6410
Jordi Planas <jorvixsky@proton.me>
2023-09-04 13:42:28 +0200

    Added Header

--- apps/web/src/components/shared/Header.tsx
+++ apps/web/src/components/shared/Header.tsx
@@ -1,12 +1,19 @@
-import { Flex, Box } from "@chakra-ui/react"
+import { Text, Box, HStack, Image } from "@chakra-ui/react"
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 
 export default function Header() {
-    return(
-        <Box>
-            <Flex>
-                <ConnectButton />
-            </Flex>
+    return (
+        <Box padding="64px">
+            <HStack justifyContent="space-between">
+                <Image src="../../..//public/HeaderLogo.svg" />
+                <HStack spacing="32px" justifyContent="space-between">
+                    <Text fontSize="16px">About ABC</Text>
+                    <Text fontSize="16px">FAQs</Text>
+                    <Text fontSize="16px">ABC Launchpad</Text>
+                    <Text fontSize="16px">ABC Swap</Text>
+                    <ConnectButton />
+                </HStack>
+            </HStack>
         </Box>
     )
 }
