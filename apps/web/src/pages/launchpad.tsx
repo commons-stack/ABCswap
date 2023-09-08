3746c06cccc9a945a1dc7d22946c013cce78251a
Jordi Planas <jordi.planas95@gmail.com>
2023-09-08 19:08:26 +0200

    Applied styles to launchpad (pending minor details)

--- apps/web/src/pages/launchpad.tsx
+++ apps/web/src/pages/launchpad.tsx
@@ -1,20 +1,30 @@
-import { Box, Divider, HStack, Text, VStack, Image } from '@chakra-ui/react';
+import { Divider, HStack, Text, VStack, Image, Button } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import './launchpad.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Launchpad() {
+
+    const navigate = useNavigate();
+
+    const handleCreateNewDAOButton = () => {
+        navigate('/launchpad/new-dao');
+    }
+
     return (
-        <VStack>
-            <Text color="brand.900" fontSize="72px" fontFamily="VictorSerifTrial">Welcome to ABC Launchpad!</Text>
-            <Text color="brand.900" fontSize="24px">Let's start creating your DAO with Augmented</Text>
-            <Text color="brand.900" fontSize="24px">Bonding Curve</Text>
-            <Box pt="80px">
-                <Divider
+        <VStack bg="brand.100" pb="100px">
+            <VStack spacing={0}>
+                <Text color="brand.900" fontSize="72px" fontFamily="VictorSerifTrial">Welcome to ABC Launchpad!</Text>
+                <Text color="brand.900" fontSize="24px" pt="32px">Let's start creating your DAO with Augmented</Text>
+                <Text color="brand.900" fontSize="24px">Bonding Curve</Text>
+                <Divider paddingTop="48px"
                     borderColor="brand.900"
                     borderBottomWidth="1px"
                     width="100%"
                     margin="0 auto"
                 />
-            </Box>
-            <HStack spacing={24}>
+            </VStack>
+            <HStack spacing={20} paddingTop="40px" >
                 <HStack>
                     <Image src="../../..//public/launchpad/ContinuousFunding.svg" />
                     <Text color="brand.900" fontSize="18px">Continuous funding</Text>
@@ -30,6 +40,20 @@ export default function Launchpad() {
                     <Image src="../../..//public/launchpad/SustainableGrowth.svg" />
                     <Text color="brand.900" fontSize="18px">Sustainable growth</Text>
                 </HStack>
+            </HStack>
+            <HStack spacing={16} pt="50px">
+                <VStack className="option-box" bgColor="white" spacing={0}>
+                    <Text color="brand.900" fontSize="40px" fontFamily="VictorSerifTrial">Create a new DAO</Text>
+                    <Text color="brand.900" fontSize="20px" mt="34px">I want to create a new DAO with an</Text>
+                    <Text color="brand.900" fontSize="20px">Augmented Bonding Curve</Text>
+                    <Button mt="34px" onClick={handleCreateNewDAOButton}>Create new DAO <ArrowForwardIcon ml="5px" /></Button>
+                </VStack>
+                <VStack className="option-box" bgColor="white" spacing={0}>
+                    <Text color="brand.900" fontSize="40px" fontFamily="VictorSerifTrial">I already have a DAO</Text>
+                    <Text color="brand.900" fontSize="20px" mt="34px">I already have a DAO and I want to</Text>
+                    <Text color="brand.900" fontSize="20px">add an Augmented Bonding Curve</Text>
+                    <Button mt="34px">Create new DAO <ArrowForwardIcon ml="5px" /></Button>
+                </VStack>
             </HStack>
         </VStack>
     )
