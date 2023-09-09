3e1187e96c9f33ec26ecf7a250afb6a0e3a76488
Jordi Planas <jordi.planas95@gmail.com>
2023-09-09 15:58:33 +0200

    Updated custom button style

--- apps/web/src/theme/components/button.ts
+++ apps/web/src/theme/components/button.ts
@@ -1,24 +1,25 @@
-// theme/components/button.js
 const Button = {
-    variants: {
-      "solid": (props: AsyncGeneratorFunctionConstructor) => ({
-        bgColor: "brand.900",
-        padding: "8px 20px",
-        borderRadius: "20px",
-        color: "brand.500",
-      }),
-      "outline": (props: AsyncGeneratorFunctionConstructor) => ({
-        borderColor: "brand.900",
-        padding: "8px 20px",
-        borderRadius: "20px",
-        color: "brand.900",
-      }),
-    },
-    defaultProps: {
-      size: "md",
-      variant: "solid",
-    },
-  };
-  
-  export default Button;
-  
+  variants: {
+    "solid": (props: AsyncGeneratorFunctionConstructor) => ({
+      bgColor: "brand.900",
+      padding: "8px 20px",
+      borderRadius: "20px",
+      color: "brand.500",
+      w: "270px",
+    }),
+    "outline": (props: AsyncGeneratorFunctionConstructor) => ({
+      borderColor: "brand.900",
+      borderWidth: "1px",  // <- Add this line
+      padding: "8px 20px",
+      borderRadius: "20px",
+      color: "brand.900",
+      w: "270px",
+    }),
+  },
+  defaultProps: {
+    size: "md",
+    variant: "solid",
+  },
+};
+
+export default Button;
