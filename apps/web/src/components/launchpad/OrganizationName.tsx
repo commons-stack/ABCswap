35f30c2c88bbb0dc7bc274a7c885b24099b702c0
Jordi Planas <jordi.planas95@gmail.com>
2023-09-15 14:19:04 +0200

    Styles name choosing step

--- apps/web/src/components/launchpad/OrganizationName.tsx
+++ apps/web/src/components/launchpad/OrganizationName.tsx
@@ -1,36 +1,38 @@
 import { Box, VStack, Text, Input, InputGroup, InputRightAddon, Alert, AlertIcon, HStack } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface OrganizationNameProps {
-    onOrganizationNameChanged: (data: string) => void;
+    onOrganizationNameChanged: (data: { organizationName: string, completed: boolean }) => void;
 }
 
-export default function OrganizationName({onOrganizationNameChanged}: OrganizationNameProps) {
+
+export default function OrganizationName({ onOrganizationNameChanged }: OrganizationNameProps) {
 
     const [organizationName, setOrganizationName] = useState<string>('');
 
-    return(
-        <main>
-            <Box borderWidth="1px" borderRadius="lg" padding="6" boxShadow="lg" width="50vw">
-                <VStack spacing={4}>
-                    <Text fontSize="xl" as="b" p="1rem" textAlign="center">Claim a name</Text>
-                    <InputGroup>
-                        <Input placeholder="Type an organization name" value={organizationName ?? ''} onChange={(e) => {
-                            onOrganizationNameChanged(e.target.value)
-                            setOrganizationName(e.target.value)
-                        }} />
-                        <InputRightAddon children='.aragonid.eth' />
-                    </InputGroup>
-                    <Alert status="info" p="1rem">
-                        <AlertIcon/>
-                        <Text fontSize="xs" as="em">Aragon uses the Ethereum Name Service (ENS) to assign names to organizations. The name you choose will be mapped to your organization’s Ethereum address and cannot be changed after you launch your organization.
-                        </Text>
-                    </Alert>
-                    <HStack>
-                    </HStack>
-                </VStack>
-            </Box>
-        </main>
+    return (
+        <Box pt="100px" pb="75px">
+            <VStack spacing={0}>
+                <Text fontFamily="VictorSerifTrial" fontSize="72px" color="brand.900">Claim a name</Text>
+                <Text fontSize="24px" color="brand.900" pt="32px">Connect your wallet to start creating your DAO</Text>
+                <Text fontSize="24px" color="brand.900">with Augmented Bonding Curve</Text>
+                <InputGroup pt="48px">
+                    <Input
+                        placeholder="Type an organization name"
+                        value={organizationName ?? ''}
+                        onChange={(e) => {
+                            setOrganizationName(e.target.value);
+                            if(organizationName.length > 0) {
+                                onOrganizationNameChanged({ organizationName, completed: true });
+                            } else {
+                                onOrganizationNameChanged({ organizationName, completed: false });
+                            }
+                        }}
+                    />
+                </InputGroup>
+
+            </VStack>
+        </Box>
     )
 
 }
