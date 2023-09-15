17d0997157a3e739837d358cf9935cbb626da448
Jordi Planas <jordi.planas95@gmail.com>
2023-09-15 14:02:52 +0200

    Created custom slider

---
+++ apps/web/src/theme/components/slider.ts
@@ -0,0 +1,25 @@
+const Slider = {
+    variants: {
+        "solid": (props: AsyncGeneratorFunctionConstructor) => ({
+            filledTrack: { 
+                bgColor: "brand.500",
+                height: "8px",
+            },
+            track: {
+                bgColor: "brand.100",
+                height: "8px",
+            },
+            thumb: {
+                bg: "brand.900",
+                height: "20px",
+                width: "20px",
+            },
+        }),
+    },
+    defaultProps: {
+        size: "md",
+        variant: "solid",
+    },
+};
+
+export default Slider;
--- apps/web/src/theme/index.ts
+++ apps/web/src/theme/index.ts
@@ -4,12 +4,16 @@ import colors from "./colors";
 import fonts from "./fonts";
 
 import Button from "./components/button";
+import Input from "./components/input";
+import Slider from "./components/slider";
 
 const theme = extendTheme({ 
     colors,
     fonts,
     components: {
         Button,
+        Input,
+        Slider,
     },
 });
 
