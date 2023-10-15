930e760bf79e119afaefa8ce5826147b0415f4b8
Pingu <pinglupingu@proton.me>
2023-10-15 11:53:25 +0200

    follow figma for home styles (swap)

--- apps/abcswap/src/pages/Home.tsx
+++ apps/abcswap/src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { Divider, HStack, Text, VStack, Image, Button, InputGroup, Input, InputRightAddon } from '@chakra-ui/react';
-import { useState } from 'react';
 import { CheckCircleIcon, WarningTwoIcon } from '@chakra-ui/icons';
+import { Button, Divider, HStack, Image, Input, InputGroup, InputRightAddon, InputRightElement, Text, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import CustomInput from '../../../../shared/src/presentation/components/CustomInput';
 // import CustomInputRightAddon from '../../../../shared/src/presentation/components/CustomInputRightAddon';
@@ -24,8 +24,8 @@ export default function Home() {
         <VStack bg="brand.100" pb="100px">
             <VStack spacing={0}>
                 <Text color="brand.900" fontSize="72px" fontFamily="VictorSerifTrial">Welcome to ABC Swap</Text>
-                <Text color="brand.900" fontSize="24px" pt="32px">Lorem ipsum</Text>
-                <Text color="brand.900" fontSize="24px">sit amet.</Text>
+                <Text color="brand.900" fontSize="24px" pt="32px">Where you can find and swap you</Text>
+                <Text color="brand.900" fontSize="24px">favorite DAO's ABC tokens.</Text>
                 <Divider paddingTop="48px"
                     borderColor="brand.900"
                     borderBottomWidth="1px"
@@ -51,21 +51,31 @@ export default function Home() {
                 </HStack>
             </HStack>
             <VStack spacing={4} mt="100px" >
-                <Text color="brand.900" fontSize="40px" fontFamily="VictorSerifTrial">Select your DAO</Text>
-                <InputGroup mt="55px">
-                    <Input 
-                        w="408px"
-                        // rightAddon={true}
+                <Text color="brand.900" fontSize="40px" fontFamily="VictorSerifTrial">Which token do you want to swap?</Text>
+                <InputGroup mt="55px" w="408px">
+                    <Input
                         placeholder="Type an organization name"
                         value={dao ?? ''}
                         autoFocus={true}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                             handleDaoChange(e.target.value);
                         }}
+                        backgroundColor={'white'}
+                        errorBorderColor='red.500'
+                        borderColor={'black'}
+                        isInvalid={userInteracted && dao.length === 0}
+                        borderRadius="15px"
+                        _hover={{'color': 'black'}}
                     />
-                    <InputRightAddon>
-                        {userInteracted ? (dao.length > 0 ? <CheckCircleIcon color="brand.500" /> : <WarningTwoIcon color="red.500" />) : ""}
-                    </InputRightAddon>
+                    <InputRightElement>
+                        {
+                            userInteracted 
+                                ? (dao.length > 0 
+                                    ? <CheckCircleIcon color="brand.500" /> 
+                                    : <WarningTwoIcon color="red.500" />) 
+                                : ""
+                        }
+                    </InputRightElement>
                 </InputGroup>
                 <Button mt="40px" disabled={!completed} w="310px" onClick={() => navigate(`/${dao}`)}>Next</Button>
             </VStack>
