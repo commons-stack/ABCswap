0e691282deb9da333c9b35bed79c3f27feb8ebe2
Pingu <pinglupingu@proton.me>
2023-10-16 14:12:24 +0200

    get namehash correctly

--- pkg/dao-utils/src/hooks/useFindDao.ts
+++ pkg/dao-utils/src/hooks/useFindDao.ts
@@ -1,22 +1,16 @@
-import { sha3_256 } from 'js-sha3';
 import { useEffect, useState } from 'react';
 import { parseAbi } from 'viem';
 import { useContractRead } from 'wagmi';
+import { Buffer } from 'buffer';
 
-function nameHash(name: string): `0x${string}` {
-  if (name === '') {
-    return '0x0000000000000000000000000000000000000000';
-  }
-  const labelHash = name.split('.')[0];
-  const firstPeriod = name.indexOf('.');
-  const remainder = firstPeriod >= 0 ? name.slice(name.indexOf('.') + 1) : "";
-  const result = sha3_256(nameHash(remainder) + sha3_256(labelHash));
-  console.log('sha3_256 of ', name, ' is ', ('0x' + result))
-  return ('0x' + result) as `0x${string}`;
-}
+// @ts-ignore
+window.Buffer = Buffer;
+
+import namehash from '@deamtest/ens-namehash';
 
 function useFindDao(name: string) {
   const [address, setAddress] = useState<(`0x${string}` | 'not_found' | 'loading' | undefined)>(undefined);
+
 
   const { data, isError, isLoading } = useContractRead({
     address: '0x6f2CA655f58d5fb94A08460aC19A552EB19909FD',
@@ -24,7 +18,7 @@ function useFindDao(name: string) {
       'function resolver(bytes32 _node) view returns (address)'
     ]),
     functionName: 'resolver',
-    args: [nameHash(name + '.aragonid.eth')]
+    args: [namehash.hash(namehash.normalize(name + '.aragonid.eth')) as `0x${string}`]
   });
 
   useEffect(() => {
@@ -43,7 +37,7 @@ function useFindDao(name: string) {
     else {
       setAddress(data);
     }
-
+    
   }, [data, isError, isLoading]);
 
   return { address };
