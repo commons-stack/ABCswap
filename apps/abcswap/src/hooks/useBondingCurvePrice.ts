047b6a33ac5886ca2a0348573e7be409ac4b4ee0
Sem <931684+sembrestels@users.noreply.github.com>
2023-10-17 01:20:06 +0200

    Improve useBondingCurvePrice hook readability

--- apps/abcswap/src/hooks/useBondingCurvePrice.ts
+++ apps/abcswap/src/hooks/useBondingCurvePrice.ts
@@ -1,7 +1,9 @@
-import { useBalance, useContractRead, useToken } from "wagmi";
+import { useBalance, useToken } from "wagmi";
+import { readContract } from '@wagmi/core';
 import { useAbcInfo } from "./useAbcInfo";
 import { useCollateral } from "./useCollateral";
 import { parseAbi } from "viem";
+import { useEffect, useState } from "react";
 
 function applyFee(amount: bigint | undefined, fee: bigint): bigint | undefined {
     if (amount === undefined) {
@@ -10,51 +12,64 @@ function applyFee(amount: bigint | undefined, fee: bigint): bigint | undefined {
     return amount - (amount * fee) / 10n ** 18n;
 }
 
+
 export function useBondingCurvePrice(amount: bigint, forwards = true, reserveToken: `0x${string}`, bondingCurve: `0x${string}`) {
+    const [price, setPrice] = useState<bigint | undefined>(undefined);
+
     const {data: info} = useAbcInfo(bondingCurve);
     const { buyFeePct, sellFeePct, token: tokenAddr, reserve: reserveAddr, formula: formulaAddr } = info;
-    const { data: collateral } = useCollateral(reserveToken, bondingCurve);
 
+    const { data: collateral } = useCollateral(reserveToken, bondingCurve);
     const { reserveRatio, virtualBalance, virtualSupply } = collateral;
 
-    const {data: abcToken } = useToken({
-        address: tokenAddr
-    })
-
+    const {data: abcToken } = useToken({ address: tokenAddr });
     const totalSupply = abcToken?.totalSupply.value;
 
-    const { data: reserveBalance } = useBalance({
-        address: reserveAddr,
-        token: reserveToken
-    })
-
+    const { data: reserveBalance } = useBalance({ address: reserveAddr, token: reserveToken });
     const balance = reserveBalance?.value;
 
     const formulaContract = {
-        address: formulaAddr,
         abi: parseAbi([
             "function calculatePurchaseReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _depositAmount) public view returns (uint256)",
             "function calculateSaleReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _sellAmount) public view returns (uint256)"
         ])
-    }
+    };
 
-    if (forwards) {
-        const { data: purchaseReturn } = useContractRead({
-            ...formulaContract,
-            functionName: 'calculatePurchaseReturn',
-            args: [(totalSupply??BigInt(0)) + (virtualSupply??BigInt(0)), 
-                (balance??BigInt(0)) + (virtualBalance??BigInt(0)), 
-                reserveRatio??0, applyFee(amount, buyFeePct??BigInt(0))!]
-        })
-        return purchaseReturn
-    } else {
-        const { data: saleReturn } = useContractRead({
-            ...formulaContract,
-            functionName: 'calculateSaleReturn',
-            args: [(totalSupply??BigInt(0)) + (virtualSupply??BigInt(0)), 
-                (balance??BigInt(0)) + (virtualBalance??BigInt(0)),
-                reserveRatio??0, amount]
-        })
-        return applyFee(saleReturn, sellFeePct??BigInt(0));
-    }
+    useEffect(() => {
+        if (
+            buyFeePct !== undefined &&
+            sellFeePct !== undefined &&
+            tokenAddr !== undefined &&
+            reserveAddr !== undefined &&
+            formulaAddr !== undefined &&
+            reserveRatio !== undefined &&
+            virtualBalance !== undefined &&
+            virtualSupply !== undefined &&
+            totalSupply !== undefined &&
+            balance !== undefined &&
+            formulaAddr !== undefined
+        ) {
+            (async () => {
+                if (forwards) {
+                    const purchaseReturn = await readContract({
+                        ...formulaContract,
+                        address: formulaAddr,
+                        functionName: 'calculatePurchaseReturn',
+                        args: [totalSupply + virtualSupply, balance + virtualBalance, reserveRatio, applyFee(amount, buyFeePct)!]
+                    })
+                    setPrice(purchaseReturn);
+                } else {
+                    const saleReturn = await readContract({
+                        ...formulaContract,
+                        address: formulaAddr,
+                        functionName: 'calculateSaleReturn',
+                        args: [totalSupply + virtualSupply, balance + virtualBalance, reserveRatio, amount]
+                    })
+                    setPrice(applyFee(saleReturn, sellFeePct));
+                }
+            })();
+        }
+    }, [buyFeePct, sellFeePct, tokenAddr, reserveAddr, formulaAddr, reserveRatio, virtualBalance, virtualSupply, totalSupply, balance, amount, forwards]);
+
+    return price;
 }
