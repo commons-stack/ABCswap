fd1a4286b72413911365d1103a3e0c3b86d80f8f
Jordi Planas <hola@jordiplanas.cat>
2023-10-17 21:24:57 +0200

    Limited max and min support & quorum

--- apps/abclaunch/src/components/dao-steps/ConfigureVoting.tsx
+++ apps/abclaunch/src/components/dao-steps/ConfigureVoting.tsx
@@ -3,7 +3,7 @@ import { InfoOutlineIcon } from '@chakra-ui/icons';
 import { newDaoVotingState } from "../../recoil";
 import { useRecoilState } from "recoil";
 
-function SliderControl({name, value, onChange, tooltip}: {name: string, tooltip: string, value: string, onChange: (value: string) => void }) {
+function SliderControl({ name, value, onChange, tooltip }: { name: string, tooltip: string, value: string, onChange: (value: string) => void }) {
     return (
         <FormControl pt="34px" pb="32px">
             <FormLabel>
@@ -29,6 +29,8 @@ function SliderControl({name, value, onChange, tooltip}: {name: string, tooltip:
                 </Slider>
                 <InputGroup width="20%" ml="10px">
                     <Input
+                        min={0}
+                        max={100}
                         value={value}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
                         type="number"
@@ -40,7 +42,7 @@ function SliderControl({name, value, onChange, tooltip}: {name: string, tooltip:
     )
 }
 
-export default function ConfigureVoting() { 
+export default function ConfigureVoting() {
 
     const [votingSettings, setVotingSettings] = useRecoilState(newDaoVotingState);
 
@@ -55,8 +57,8 @@ export default function ConfigureVoting() {
                 margin="0 auto"
             />
             <VStack width="90%">
-                <SliderControl name="SUPPORT" value={votingSettings.supportRequired} onChange={supportRequired => setVotingSettings(settings => ({...settings, supportRequired}))} tooltip="Support is the relative percentage of tokens that are required to vote “Yes” for a proposal to be approved. For example, if “Support” is set to 50%, then more than 50% of the tokens used to vote on a proposal must vote “Yes” for it to pass." />
-                <SliderControl name="MINIMUM APPROVAL" value={votingSettings.minimumAcceptanceQuorum} onChange={minimumAcceptanceQuorum => setVotingSettings(settings => ({...settings, minimumAcceptanceQuorum}))} tooltip="Minimum approval is the percentage of the total token supply that is required to vote “Yes” on a proposal before it can be approved. For example, if the “Minimum Approval” is set to 20%, then more than 20% of the outstanding token supply must vote “Yes” on a proposal for it to pass." />
+                <SliderControl name="SUPPORT" value={votingSettings.supportRequired} onChange={supportRequired => Number(supportRequired) <= 100 && Number(supportRequired) >= 0 && setVotingSettings(settings => ({ ...settings, supportRequired }))} tooltip="Support is the relative percentage of tokens that are required to vote “Yes” for a proposal to be approved. For example, if “Support” is set to 50%, then more than 50% of the tokens used to vote on a proposal must vote “Yes” for it to pass." />
+                <SliderControl name="MINIMUM APPROVAL" value={votingSettings.minimumAcceptanceQuorum} onChange={minimumAcceptanceQuorum => Number(minimumAcceptanceQuorum) <= 100 && Number(minimumAcceptanceQuorum) >= 0 && setVotingSettings(settings => ({ ...settings, minimumAcceptanceQuorum }))} tooltip="Minimum approval is the percentage of the total token supply that is required to vote “Yes” on a proposal before it can be approved. For example, if the “Minimum Approval” is set to 20%, then more than 20% of the outstanding token supply must vote “Yes” on a proposal for it to pass." />
                 <HStack alignSelf="start" mt="38px">
                     <Text fontSize="16px" color="brand.900">VOTE DURATION</Text>
                     <Tooltip label="Vote duration is the length of time that the vote will be open for participation. For example, if the Vote Duration is set to 24 hours, then token holders have 24 hours to participate in the vote.">
@@ -69,7 +71,7 @@ export default function ConfigureVoting() {
                             <InputGroup key={index} variant="textRTL">
                                 <Input
                                     value={(votingSettings[`voteDuration${key}` as 'voteDurationDays' | 'voteDurationHours' | 'voteDurationMinutes'])}
-                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVotingSettings(settings => ({...settings, [`voteDuration${key}`]: e.target.value}))}
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVotingSettings(settings => ({ ...settings, [`voteDuration${key}`]: e.target.value }))}
                                 />
                                 <InputRightElement children={key} />
                             </InputGroup>
