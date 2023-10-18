02e0ef212609d952e36b1baf2257ef7f835a1dc6
Jordi Planas <jorvixsky@proton.me>
2023-10-18 11:57:31 +0200

    Removed left text

--- pkg/commons-ui/src/components/Header.tsx
+++ pkg/commons-ui/src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header({headerLocation}: HeaderProps) {
     return (
         <Box padding="64px">
             <HStack justifyContent="space-between">
-                <Text fontSize="xl">ABC Swap</Text>
+                <Text fontSize="xl"></Text> {/*Left blank to keep content in place till we add logo or text again*/}
                 <HStack spacing="32px" justifyContent="space-between">
                 <Text fontSize="16px">ABC {headerLocation === 'swap' ? 'Swap' : 'Launchpad'}</Text>
                     <Link href="https://www.commonsstack.org/augmented-bonding-curve" isExternal fontSize="16px">About ABC</Link>
