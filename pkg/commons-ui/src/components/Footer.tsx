fd78eb644ccd08ceba29b131290306bb1a35be7b
Jordi Planas <jorvixsky@proton.me>
2023-10-18 16:13:35 +0200

    Footer WIP

--- pkg/commons-ui/src/components/Footer.tsx
+++ pkg/commons-ui/src/components/Footer.tsx
@@ -1,13 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack, Image, Text } from "@chakra-ui/react";
 
 export default function Footer() {
     return (
         <Box
             width="100%"
-            padding="80px"
+            padding="60px"
             bgColor="brand.900"
             textAlign="center"
         >
+            <HStack justifyContent="space-between">
+                <HStack>
+                    <Text fontSize="16px" fontFamily="Victor Serif Trial" color="brand.500">Powered by</Text>
+                    <Image src="public/CSLogo.svg" />
+                </HStack>
+                <Text fontSize="14px" color="brand.500">2023 ABC Labs. All rights reserved.</Text>
+            </HStack>
         </Box>
     )
 }
