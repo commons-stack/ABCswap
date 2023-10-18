d11be517a222f7c8e5345687b21117e94e116843
Jordi Planas <jorvixsky@proton.me>
2023-10-18 12:03:10 +0200

    Added links to go to swap/launch, fixed wrongfully painted

--- pkg/commons-ui/src/components/Header.tsx
+++ pkg/commons-ui/src/components/Header.tsx
@@ -5,13 +5,21 @@ type HeaderProps = {
     headerLocation: "swap" | "launchpad"
 }
 
-export default function Header({headerLocation}: HeaderProps) {
+export default function Header({ headerLocation }: HeaderProps) {
+
+    const swapOrLaunchLink = headerLocation === "launchpad" ? "https://abcswap.xyz/" : "https://launch.abcswap.xyz/";
+
     return (
         <Box padding="64px">
             <HStack justifyContent="space-between">
                 <Text fontSize="xl"></Text> {/*Left blank to keep content in place till we add logo or text again*/}
                 <HStack spacing="32px" justifyContent="space-between">
-                <Text fontSize="16px">ABC {headerLocation === 'swap' ? 'Swap' : 'Launchpad'}</Text>
+                    <Link
+                        href={swapOrLaunchLink}
+                        fontSize="16px"
+                        isExternal
+                    >ABC {headerLocation === 'launchpad' ? 'Swap' : 'Launchpad'}
+                    </Link>
                     <Link href="https://www.commonsstack.org/augmented-bonding-curve" isExternal fontSize="16px">About ABC</Link>
                     <ConnectButton />
                 </HStack>
