945fc56865977d19c71685c6a42d630beb1433c9
Sem <931684+sembrestels@users.noreply.github.com>
2023-10-19 22:28:07 +0200

    Check input validity on voting settings

--- apps/abclaunch/src/components/dao-steps/ConfigureVoting.tsx
+++ apps/abclaunch/src/components/dao-steps/ConfigureVoting.tsx
@@ -1,14 +1,21 @@
-import { InputGroup, Input, InputRightElement, Text, VStack, Divider, FormControl, FormLabel, HStack, Tooltip, Slider, SliderTrack, SliderFilledTrack, SliderThumb } from "@chakra-ui/react";
+import { InputGroup, Input, InputRightElement, Text, VStack, Divider, FormControl, FormLabel, HStack, Tooltip, Slider, SliderTrack, SliderFilledTrack, SliderThumb, NumberInput, NumberInputField } from "@chakra-ui/react";
 import { InfoOutlineIcon } from '@chakra-ui/icons';
 import { newDaoVotingState } from "../../recoil";
 import { useRecoilState } from "recoil";
 
-function SliderControl({ name, value, onChange, tooltip }: { name: string, tooltip: string, value: string, onChange: (value: string) => void }) {
+function SliderControl({ name, title, tooltip }: { name: 'supportRequired' | 'minimumAcceptanceQuorum', title: string, tooltip: string }) {
+    const [votingSettings, setVotingSettings] = useRecoilState(newDaoVotingState);
+    const value = votingSettings[name];
+
+    function onChange(value: string) {
+        /^\d*\.?\d*$/.test(value) && Number(value) <= 100 && Number(value) >= 0 && setVotingSettings(settings => ({ ...settings, [name]: value }))
+    }
+
     return (
         <FormControl pt="17px" pb="16px">
             <FormLabel>
                 <HStack>
-                    <Text fontSize="16px" color="brand.900">{name}</Text>
+                    <Text fontSize="16px" color="brand.900">{title}</Text>
                     <Tooltip label={tooltip}>
                         <InfoOutlineIcon />
                     </Tooltip>
@@ -26,13 +33,9 @@ function SliderControl({ name, value, onChange, tooltip }: { name: string, toolt
                     <SliderThumb />
                 </Slider>
                 <InputGroup width="20%" ml="10px">
-                    <Input
-                        min={0}
-                        max={100}
-                        value={value}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
-                        type="number"
-                    />
+                    <NumberInput value={value}>
+                        <NumberInputField onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)} border="1px" _hover={{ border: "1px" }} _focusVisible={{boxShadow: 'none'}} />
+                    </NumberInput>
                     <InputRightElement children="%" />
                 </InputGroup>
             </HStack>
@@ -40,10 +43,25 @@ function SliderControl({ name, value, onChange, tooltip }: { name: string, toolt
     )
 }
 
-export default function ConfigureVoting() {
-
+function TimeInput ({ timeUnit }: {timeUnit: 'Days' | 'Hours' | 'Minutes'}) {
     const [votingSettings, setVotingSettings] = useRecoilState(newDaoVotingState);
 
+    function handleTimeChange (time: string) {
+        /^\d*\.?\d*$/.test(time) && setVotingSettings(settings => ({ ...settings, [`voteDuration${timeUnit}`]: time }))
+    }
+
+    return (
+        <InputGroup variant="textRTL">
+            <Input
+                value={(votingSettings[`voteDuration${timeUnit}`])}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleTimeChange(e.target.value)}
+            />
+            <InputRightElement children={timeUnit} />
+        </InputGroup>
+    )
+}
+
+export default function ConfigureVoting() {
     return (
         <VStack spacing={4} mt="75" className="abcs-newdao-step-content">
             <Text fontFamily="VictorSerifTrial" fontSize="72px" color="brand.900">Voting</Text>
@@ -55,8 +73,8 @@ export default function ConfigureVoting() {
                 margin="0 auto"
             />
             <VStack width="90%">
-                <SliderControl name="SUPPORT" value={votingSettings.supportRequired} onChange={supportRequired => Number(supportRequired) <= 100 && Number(supportRequired) >= 0 && setVotingSettings(settings => ({ ...settings, supportRequired }))} tooltip="Support is the relative percentage of tokens that are required to vote “Yes” for a proposal to be approved. For example, if “Support” is set to 50%, then more than 50% of the tokens used to vote on a proposal must vote “Yes” for it to pass." />
-                <SliderControl name="MINIMUM APPROVAL" value={votingSettings.minimumAcceptanceQuorum} onChange={minimumAcceptanceQuorum => Number(minimumAcceptanceQuorum) <= 100 && Number(minimumAcceptanceQuorum) >= 0 && setVotingSettings(settings => ({ ...settings, minimumAcceptanceQuorum }))} tooltip="Minimum approval is the percentage of the total token supply that is required to vote “Yes” on a proposal before it can be approved. For example, if the “Minimum Approval” is set to 20%, then more than 20% of the outstanding token supply must vote “Yes” on a proposal for it to pass." />
+                <SliderControl title="SUPPORT" name="supportRequired" tooltip="Support is the relative percentage of tokens that are required to vote “Yes” for a proposal to be approved. For example, if “Support” is set to 50%, then more than 50% of the tokens used to vote on a proposal must vote “Yes” for it to pass." />
+                <SliderControl title="MINIMUM APPROVAL" name="minimumAcceptanceQuorum" tooltip="Minimum approval is the percentage of the total token supply that is required to vote “Yes” on a proposal before it can be approved. For example, if the “Minimum Approval” is set to 20%, then more than 20% of the outstanding token supply must vote “Yes” on a proposal for it to pass." />
                 <HStack alignSelf="start" mt="16px">
                     <Text fontSize="16px" color="brand.900">VOTE DURATION</Text>
                     <Tooltip label="Vote duration is the length of time that the vote will be open for participation. For example, if the Vote Duration is set to 24 hours, then token holders have 24 hours to participate in the vote.">
@@ -64,17 +82,9 @@ export default function ConfigureVoting() {
                     </Tooltip>
                 </HStack>
                 <HStack spacing={4}>
-                    {['Days', 'Hours', 'Minutes'].map((key, index) => {
-                        return (
-                            <InputGroup key={index} variant="textRTL">
-                                <Input
-                                    value={(votingSettings[`voteDuration${key}` as 'voteDurationDays' | 'voteDurationHours' | 'voteDurationMinutes'])}
-                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVotingSettings(settings => ({ ...settings, [`voteDuration${key}`]: e.target.value }))}
-                                />
-                                <InputRightElement children={key} />
-                            </InputGroup>
-                        )
-                    })}
+                    <TimeInput timeUnit="Days" />
+                    <TimeInput timeUnit="Hours" />
+                    <TimeInput timeUnit="Minutes" />
                 </HStack>
             </VStack>
             <Divider paddingTop="12px"
