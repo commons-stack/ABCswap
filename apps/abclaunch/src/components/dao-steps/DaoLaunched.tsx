c395329b2c7bac022e5cdbeca7d29b77a6fa34a4
Jordi Planas <jorvixsky@proton.me>
2023-10-20 09:02:27 +0200

    Styled congratulations page

--- apps/abclaunch/src/components/dao-steps/DaoLaunched.tsx
+++ apps/abclaunch/src/components/dao-steps/DaoLaunched.tsx
@@ -1,19 +1,20 @@
 import { Box, Button, HStack, Text, VStack } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import { newDaoCreatedUrl } from "../../recoil";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function DaoLaunched() {
 
     const createdDaoUrl = useRecoilValue(newDaoCreatedUrl);
 
     return (
-        <Box pt="100px" pb="75px">
+        <Box pt="75px" pb="75px">
             <VStack spacing={0} className="abcs-newdao-step-content">
-                <Text mb="48px" fontFamily="VictorSerifTrial" fontSize="72px" color="brand.900">Congratulations!</Text>
-                <Text>Your DAO and ABC were created.</Text>
-                <HStack>
-                    <Button variant="outline" onClick={() => window.open("http://localhost:3000", '_blank')}>Go to ABC Swap</Button>
-                    <Button variant="outline" onClick={() => window.open(createdDaoUrl, '_blank')}>Go to my DAO</Button>
+                <Text fontFamily="VictorSerifTrial" fontSize="72px" color="brand.900">Congratulations!</Text>
+                <Text mt="32px" fontSize="24px" color="brand.900" >Your DAO and ABC were created.</Text>
+                <HStack mt="28px" spacing={5}>
+                    <Button onClick={() => window.open("https://abcswap.xyz", '_blank')}>Go to ABC Swap <ArrowForwardIcon /></Button>
+                    <Button onClick={() => window.open(createdDaoUrl, '_blank')}>Go to my DAO <ArrowForwardIcon/></Button>
                 </HStack>
             </VStack>
         </Box>
