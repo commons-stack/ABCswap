af12dcbcb69941d81ba58c38040d3d0182179cd8
Jordi Planas <jorvixsky@proton.me>
2023-10-20 16:23:40 +0200

    Add banner for MVP test version

--- pkg/commons-ui/src/components/Header.tsx
+++ pkg/commons-ui/src/components/Header.tsx
@@ -10,20 +10,27 @@ export default function Header({ variant }: HeaderProps) {
     const swapOrLaunchLink = variant === "launch" ? "https://abcswap.xyz/" : "https://launch.abcswap.xyz/";
 
     return (
-        <Box padding="64px">
-            <HStack justifyContent="space-between">
-                <Text fontSize="xl"></Text> {/*Left blank to keep content in place till we add logo or text again*/}
-                <HStack spacing="32px" justifyContent="space-between">
-                    <Link
-                        href={swapOrLaunchLink}
-                        fontSize="16px"
-                        isExternal
-                    >ABC {variant === 'launch' ? 'Swap' : 'Launch'}
-                    </Link>
-                    <Link href="https://www.commonsstack.org/augmented-bonding-curve" isExternal fontSize="16px">About ABC</Link>
-                    <ConnectButton />
+        <>
+            <Box pt="16px" pb="16px" backgroundColor="brand.500">
+                <Text textAlign="center">This is an MVP of ABC Swap and Launch. Send a note to info@abcswap.xyz with any issues you encounter.</Text>
+            </Box>
+            <Box padding="64px">
+
+                <HStack justifyContent="space-between">
+                    <Text fontSize="xl"></Text> {/*Left blank to keep content in place till we add logo or text again*/}
+                    <HStack spacing="32px" justifyContent="space-between">
+                        <Link
+                            href={swapOrLaunchLink}
+                            fontSize="16px"
+                            isExternal
+                        >ABC {variant === 'launch' ? 'Swap' : 'Launch'}
+                        </Link>
+                        <Link href="https://www.commonsstack.org/augmented-bonding-curve" isExternal fontSize="16px">About ABC</Link>
+                        <ConnectButton />
+                    </HStack>
                 </HStack>
-            </HStack>
-        </Box>
+            </Box>
+        </>
+
     )
 }
